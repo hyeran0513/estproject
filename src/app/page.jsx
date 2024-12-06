43cea85364b573b63a44613109d7889e23bfe1e5
hyeran0513 <hr_0513@naver.com>
2024-12-06 17:01:42 +0900

    241206 김혜란 page.jsx 들여쓰기 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,16 +5,19 @@ import { BiHeartCircle } from "react-icons/bi";
 export default function Home() {
   return (
     <div className={styles.page}>
-        <h1>이스트소프트 프론트엔드 부트캠프<BiHeartCircle /></h1>
+      <h1>
+        이스트소프트 프론트엔드 부트캠프
+        <BiHeartCircle />
+      </h1>
 
-        <Image
-          className={styles.logo}
-          src={`${process.env.ASSET_PREFIX}/images/cat.jpg`}
-          alt="cat"
-          width={200}
-          height={200}
-          priority
-        />
+      <Image
+        className={styles.logo}
+        src={`${process.env.ASSET_PREFIX}/images/cat.jpg`}
+        alt="cat"
+        width={200}
+        height={200}
+        priority
+      />
     </div>
   );
 }
