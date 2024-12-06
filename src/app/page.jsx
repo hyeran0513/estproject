43bc4d8dcd6126e6c4cb5bfde4d4985549d43ee5
sejin <neokin7734@naver.com>
2024-12-06 18:04:12 +0900

    2024-12-06 박세진 page.jsx 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
         <h1>이스트소프트 프론트엔드 부트캠프<BiHeartCircle /></h1>
-
+        next.js, scss 처음이에요... 잘부탁해요...
         <Image
           className={styles.logo}
           src={`${process.env.ASSET_PREFIX}/images/cat.jpg`}
