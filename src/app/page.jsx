daaecd691527f817133c7baff6deade229a723a6
hyeran0513 <hr_0513@naver.com>
2024-12-06 18:13:48 +0900

    241206 김혜란 page.jsx 내용 정리

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,6 @@ import { BiHeartCircle } from "react-icons/bi";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>이스트소프트 프론트엔드 부트캠프<BiHeartCircle /></h1>
-      next.js, scss 처음이에요... 잘부탁해요...
-
       <h1>
         이스트소프트 프론트엔드 부트캠프
         <BiHeartCircle />
@@ -21,17 +18,10 @@ export default function Home() {
         height={200}
         priority
       />
-      
-      <h2>안녕하세요 형주희입니다 잘부탁드립니다!</h2>
 
-      <Image
-        className={styles.logo}
-        src={`${process.env.ASSET_PREFIX}/images/cat.jpg`}
-        alt="cat"
-        width={200}
-        height={200}
-        priority
-      />
+      <div>next.js, scss 처음이에요... 잘부탁해요...</div>
+
+      <h2>안녕하세요 형주희입니다 잘부탁드립니다!</h2>
 
       <p>안녕하세요 최승이입니다 잘부탁드립니다!!!! </p>
     </div>
